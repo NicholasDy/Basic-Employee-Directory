f580bf492b44d7a08050aea0c20deb6f19d47ba1
Nick <nicholasdy272@gmail.com>
2021-04-21 22:14:37 -0400

    view all by department and roles are set, 3/4 of adding new employee are done

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,13 @@
 const inquirer = require("inquirer")
 const mysql = require("mysql")
-const cTable = require("console.table")
+const cTable = require("console.table");
+const e = require("express");
 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: , //insert password,
+    password:  //insert password,
     database: 'directorydb',
 });
 
@@ -40,7 +41,7 @@ function mainPage() {
             type: "list",
             name: "main",
             message: "What would you like to do?",
-            choices: ["View all Employees", "View all Employees by Department", "View all Employees by Role", "Add Employee", "Update Employee", "Update Roles", "Update Departments"]
+            choices: ["View all Employees", "View all Employees by Department", "View all Employees by Role", "Add Employee", "Update Employee", "Add Roles", "Add Departments"]
         }
     ])
         .then(answers => {
@@ -55,9 +56,11 @@ function mainPage() {
                     viewEmpRole()
                     break;
                 case "Add Employee":
+                    addingEmp()
+                    break;
                 case "Update Employee":
-                case "Update Roles":
-                case "Update Departments":
+                case "Add Roles":
+                case "Add Departments":
             };
         })
 };
@@ -66,54 +69,129 @@ function mainPage() {
 // The questions for what the user wants to do are going to appear on the terminal 
 //switch statment 
 //Each of the seperate tasks is going to need a function for a total of 6 basic functions 
+// SELECT
+// FROM employee AS e
+// JOIN roles AS r
+// JOIN department AS d
 
-
-function viewAllEmp() {
-    connection.query("SELECT * FROM employee;",
-        (err, res) => {
-            console.table(res);
-            mainPage();
-        }
+function viewAllEmp() { //might want to add more info in the table when it comes out see the readme
+    connection.query(`
+    SELECT
+        e.firstName, 
+        e.lastName, 
+        r.salary,
+        r.title,
+        d.departID, 
+        CONCAT(m.firstName,' ', m.lastName) AS Manager
+    FROM employee AS e
+    JOIN roles AS r
+        ON e.roleID = r.idRole
+    JOIN department AS d
+        ON r.department_id = d.idDepart 
+    LEFT JOIN employee AS m ON m.idEmp = e.managerID
+    `, (err, res) => {
+        if (err) throw err
+        console.table(res);
+        mainPage();
+    }
     )
 };
 
 function viewEmpDepart() {
-    connection.query("SELECT departID FROM department",
-        (err, res) => {
-            let listOfOptions = [] 
-            for(let i = 0; i < res.length; i++ ){
-                let resPush = res[i].departID
-                listOfOptions.push(resPush)
-            }
-            inquirer.prompt([
-                {
-                    type: "list",
-                    message: "Which department would you like to see?",
-                    name:"depart",
-                    choices: listOfOptions
-                }
-            ])
-            .then(answers => {
-                connection.query(`
-                SELECT 
-                    * 
-                FROM 
-                    employee AS e
-                INNER JOIN
-                    department AS d
-                WHERE
-                    d.departID = ${answers.depart}
-                    `,
-                (err, res) => {
-                    console.table(res);
-                    mainPage();
-                }
-            )
-            })
-            
-        })
-};
+    connection.query(`
+    SELECT 
+        e.firstName, 
+        e.lastName, 
+        d.departID AS Department
+    FROM employee AS e
+    JOIN roles AS r
+        ON e.roleID = r.idRole
+    JOIN department AS d
+        ON r.department_id = d.idDepart ORDER BY e.roleID;`, (err, res) => {
+        if (err) throw err
+        console.table(res)
+        mainPage()
+    })
+}
 
 function viewEmpRole() {
-
+    connection.query(`
+    SELECT
+        e.firstName,
+        e.lastName, 
+        r.title
+    FROM employee AS e
+    INNER JOIN roles AS r
+        ON e.roleID = r.idRole ORDER BY r.idRole
+    `, (err, res) => {
+        if (err) throw err
+        console.table(res)
+        mainPage()
+    })
 };
+
+function addingEmp() {
+    console.log('prepare to add in new employee information')
+    connection.query(`
+    SELECT 
+        r.title 
+    FROM roles AS r`,
+        (err, res) => {
+            let listOfOptionsRoles = []
+            for (let i = 0; i < res.length; i++) {
+                let resPush = res[i].title
+                listOfOptionsRoles.push(resPush)
+            }
+            connection.query(`
+    SELECT
+        CONCAT(e.firstName, ' ', e.lastName) AS Man
+    FROM employee AS e
+    `, (err, res) => {
+                listOfOptionsMan = []
+                for (let i = 0; i < res.length; i++) {
+                    let resPush = res[i].Man
+                    listOfOptionsMan.push(resPush)
+                }
+                listOfOptionsMan.push('--')
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "fname",
+                        message: "What is your employee's first name?",
+                    },
+                    {
+                        type: "input",
+                        name: "lname",
+                        message: "What is your employee's last name?",
+                    },
+                    {
+                        type: "list",
+                        name: "roles",
+                        message: "What is your employee's Role?",
+                        choices: listOfOptionsRoles,
+                    },
+                    {
+                        type: "list",
+                        name: "manager",
+                        message: "What is your employee's Manager?",
+                        choices: listOfOptionsMan,
+                    },
+                ]).then(({ fname, lname, roles, manager }) => {
+                    console.log(manager)
+
+                    connection.query(`
+                    INSERT INTO employee 
+                        (firstName, lastName, roleID, managerID) 
+                    VALUES
+                        (${fname}, ${lname}, (SELECT idRole FROM roles WHERE title = ${roles}), 
+                        (SELECT * FROM (
+                            SELECT idEmp, CONCAT(firstName, " ",lastName) as firstlast
+                            FROM employees) 
+                            WHERE firstlast = ${manager})
+                    `)
+                })
+            })
+
+        })
+}
+
