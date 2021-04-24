1f4837827396f193c4da9f26d319cdb16fb10fd9
Nick <nicholasdy272@gmail.com>
2021-04-23 22:05:15 -0400

    adding finished code, intnert issues prevented previous uploads

--- Develop/seeds.sql
+++ Develop/seeds.sql
@@ -1,8 +1,7 @@
 INSERT INTO department (idDepart, departID) VALUES (1, "Sales"), (2, "Accounting"), (3, "Maintenance");
 
+INSERT INTO roles (idRole, title, salary, department_id) VALUES (1, "Sales Manager", 90000, 1), (2, "Sales Rep", 75000, 1), (3, "Accounting Manager", 10000, 2), (4, "Accountant", 90000, 2), (5, "Maintence Manager", 75000, 3), (6, "Technician", 50000, 3);
+
+INSERT INTO employee (idEmp, firstName, lastName, roleID, managerID) VALUES (1, "Nick", "Dyke", 1, Null), (2, "Josh", "Russel", 2, Null), (3, "Ann", "Katherine", 3, Null), (4, "Jim", "Russel", 4, 3), (5, "Katie", "Kei", 5, 1)
+
 -- 1 - 9 is Head management, 10 - 19 is sales, 20 - 29 is accounting, 30 - 39 Maintenance
-
-INSERT INTO roles (idRole, title, salary, department_id) VALUES (10, "Sales Manager", 90000, 1), (11, "Sales Rep", 75000, 1), (20, "Accounting Manager", 10000, 2), (21, "Accountant", 90000, 2), (30, "Maintence Manager", 75000, 3), (31, "Technician", 50000, 3);
-
-INSERT INTO employee (idEmp, firstName, lastName, roleID, managerID) VALUES (1, "Nick", "Dyke", 10, Null), (2, "Josh", "Russel", 20, Null), (3, "Ann", "Katherine", 30, Null), (4, "Jim", "Russel", 31, 3), (5, "Katie", "Kei", 11, 1)
-
--- Develop/server.js
+++ Develop/server.js
@@ -3,11 +3,16 @@ const mysql = require("mysql")
 const cTable = require("console.table");
 const e = require("express");
 
+
+let listOfOptionsRoles = [];
+let listOfOptionsMan = [];
+let listofDepart = [];
+
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password:  //insert password,
+    password: "", //insert password,
     database: 'directorydb',
 });
 
@@ -15,24 +20,9 @@ connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}`);
     mainPage();
+    getEmployee();
+    getEmployeeMan();
 })
-
-
-// inputs for the following for an employee 
-// id ( personal id, role id, department id)
-// first name 
-// last name 
-// manager ID --> who is this persons manager (can have a check list of names)
-// salary
-// department
-
-//General task 
-
-// Add departments, roles, employees
-
-// View departments, roles, employees
-
-// Update employee roles
 
 function mainPage() {
     console.log("Welcome to the company directory!")
@@ -41,7 +31,7 @@ function mainPage() {
             type: "list",
             name: "main",
             message: "What would you like to do?",
-            choices: ["View all Employees", "View all Employees by Department", "View all Employees by Role", "Add Employee", "Update Employee", "Add Roles", "Add Departments"]
+            choices: ["View all Employees", "View all Employees by Department", "View all Employees by Role", "View all Roles", "View all Departments", "Add Employee", "Add Roles", "Add Departments", "Update Employee Role"]
         }
     ])
         .then(answers => {
@@ -55,25 +45,29 @@ function mainPage() {
                 case "View all Employees by Role":
                     viewEmpRole()
                     break;
+                case "View all Roles":
+                    viewRoles()
+                    break;
+                case "View all Departments":
+                    viewDepart()
+                    break;
                 case "Add Employee":
                     addingEmp()
                     break;
-                case "Update Employee":
                 case "Add Roles":
+                    addingRoles()
+                    break;
                 case "Add Departments":
+                    addingDepart()
+                    break;
+                case "Update Employee Role":
+                    updateRoles()
+                    break;
             };
         })
 };
 
-
-// The questions for what the user wants to do are going to appear on the terminal 
-//switch statment 
-//Each of the seperate tasks is going to need a function for a total of 6 basic functions 
-// SELECT
-// FROM employee AS e
-// JOIN roles AS r
-// JOIN department AS d
-
+//Viewing functions 
 function viewAllEmp() { //might want to add more info in the table when it comes out see the readme
     connection.query(`
     SELECT
@@ -128,70 +122,224 @@ function viewEmpRole() {
         console.table(res)
         mainPage()
     })
-};
+}
 
-function addingEmp() {
-    console.log('prepare to add in new employee information')
+function viewDepart() {
     connection.query(`
-    SELECT 
-        r.title 
-    FROM roles AS r`,
+    SELECT * FROM department
+    `, (err, res) => {
+        if (err) throw err
+        console.table(res);
+        mainPage();
+    })
+}
+
+function viewRoles() {
+    connection.query(`
+    SELECT * FROM roles
+    `, (err, res) => {
+        if (err) throw err
+        console.table(res);
+        mainPage();
+    })
+}
+
+//adding functions 
+function addingEmp() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "fname",
+            message: "What is your employee's first name?",
+        },
+        {
+            type: "input",
+            name: "lname",
+            message: "What is your employee's last name?",
+        },
+        {
+            type: "list",
+            name: "roles",
+            message: "What is your employee's Role?",
+            choices: listOfOptionsRoles,
+        },
+        {
+            type: "list",
+            name: "manager",
+            message: "What is your employee's Manager?",
+            choices: listOfOptionsMan,
+        },
+    ]).then(({ fname, lname, roles, manager }) => {
+
+        let managedId = employeeID(manager)
+        let roledId = getRoleId(roles)
+        connection.query(`
+        INSERT INTO employee SET ?`,
+            {
+                firstName: fname,
+                lastName: lname,
+                managerId: managedId,
+                roleId: roledId
+
+            }, (err, res) => {
+                if (err) throw err
+                mainPage()
+            })
+    })
+}
+function addingRoles() {
+    getDepartment();
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of the Role?",
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: "What is the salary of said Role?",
+        },
+        {
+            type: "list",
+            name: "deparment",
+            message: "What is your employee's Manager?",
+            choices: listofDepart,
+        },
+    ]).then(({ title, salary, deparment }) => {
+
+        let depID = getDepartId(deparment)
+        connection.query(`
+            INSERT INTO roles SET ?`,
+            {
+                title: title,
+                salary: salary,
+                department_id: depID
+            },
+            (err, res) => {
+                if (err) throw err
+                mainPage()
+            })
+    })
+}
+function addingDepart() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of the department??",
+        },
+
+    ]).then((answers) => {
+        connection.query(`
+            INSERT INTO department SET ?`,
+            {
+                departID: answers.title,
+            },
+            (err, res) => {
+                if (err) throw err
+                mainPage()
+            })
+    })
+}
+
+//updating roles
+function updateRoles() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "person",
+            message: "Whose role are you updating?",
+            choices: listOfOptionsMan,
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "What is their new role?",
+            choices: listOfOptionsRoles,
+        }
+    ]).then(({ person, role }) => {
+        let managedId = employeeID(person)
+        let roledId = getRoleId(role)
+        connection.query(`
+        UPDATE employee
+        SET roleID = ${roledId}
+        WHERE idEmp = ${managedId}
+        `, (err, res) => {
+            if (err) throw err
+            viewEmpRole()
+        })
+
+    })
+}
+
+// getting ID functions
+
+function employeeID(manager) {
+    for (let i = 0; i < listOfOptionsMan.length; i++) {
+        if (manager == listOfOptionsMan[i]) {
+            managedId = i + 1
+        }
+    }
+    return managedId
+}
+function getRoleId(roles) {
+    for (let i = 0; i < listOfOptionsRoles.length; i++) {
+        if (roles == listOfOptionsRoles[i]) {
+            roledId = i + 1
+        }
+    }
+    return roledId
+}
+function getDepartId(deparment) {
+    console.log(listofDepart.length)
+    for (let i = 0; i < listofDepart.length; i++) {
+        if (deparment == listofDepart[i]) {
+            depID = i + 1
+            console.log(i)
+        }
+    }
+    console.log(depID)
+    return depID
+}
+
+//creating array functions 
+function getEmployee() {
+    console.log('getting roles')
+    connection.query(`
+SELECT 
+r.title 
+FROM roles AS r`,
         (err, res) => {
-            let listOfOptionsRoles = []
             for (let i = 0; i < res.length; i++) {
                 let resPush = res[i].title
                 listOfOptionsRoles.push(resPush)
             }
-            connection.query(`
+        })
+    return;
+}
+function getEmployeeMan() {
+    connection.query(`  
     SELECT
         CONCAT(e.firstName, ' ', e.lastName) AS Man
     FROM employee AS e
-    `, (err, res) => {
-                listOfOptionsMan = []
-                for (let i = 0; i < res.length; i++) {
-                    let resPush = res[i].Man
-                    listOfOptionsMan.push(resPush)
-                }
-                listOfOptionsMan.push('--')
-                inquirer.prompt([
-                    {
-                        type: "input",
-                        name: "fname",
-                        message: "What is your employee's first name?",
-                    },
-                    {
-                        type: "input",
-                        name: "lname",
-                        message: "What is your employee's last name?",
-                    },
-                    {
-                        type: "list",
-                        name: "roles",
-                        message: "What is your employee's Role?",
-                        choices: listOfOptionsRoles,
-                    },
-                    {
-                        type: "list",
-                        name: "manager",
-                        message: "What is your employee's Manager?",
-                        choices: listOfOptionsMan,
-                    },
-                ]).then(({ fname, lname, roles, manager }) => {
-                    console.log(manager)
-
-                    connection.query(`
-                    INSERT INTO employee 
-                        (firstName, lastName, roleID, managerID) 
-                    VALUES
-                        (${fname}, ${lname}, (SELECT idRole FROM roles WHERE title = ${roles}), 
-                        (SELECT * FROM (
-                            SELECT idEmp, CONCAT(firstName, " ",lastName) as firstlast
-                            FROM employees) 
-                            WHERE firstlast = ${manager})
-                    `)
-                })
-            })
-
-        })
+        `, (err, res) => {
+        for (let i = 0; i < res.length; i++) {
+            let resPush = res[i].Man
+            listOfOptionsMan.push(resPush)
+        }
+        listOfOptionsMan.push('--')
+    })
+    return;
 }
-
+function getDepartment() {
+    connection.query(`
+    SELECT departID FROM department
+    `, (err, res) => {
+        for (let i = 0; i < res.length; i++) {
+            let resPush = res[i].departID
+            listofDepart.push(resPush)
+        }
+    })
+    return;
+}
